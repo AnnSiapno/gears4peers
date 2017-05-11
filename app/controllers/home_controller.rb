6849eea34566ffeebdc666c2abbd9efd57482fb0
Ann Siapno <ann.n.siapno@gmail.com>
2017-05-11 20:11:02 +1000

    Fixed search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 
   def search_results()
     @search = '%' + params[:search].downcase + '%'
-    @results = Listing.where("title LIKE '#{@search}' OR description LIKE '#{@search}' OR city LIKE '#{@search}' OR state LIKE '#{@search}'")
+    @results = Listing.where("LOWER(title) LIKE '#{@search}' OR LOWER(description) LIKE '#{@search}' OR LOWER(city) LIKE '#{@search}' OR LOWER(state) LIKE '#{@search}'")
     @hash = Gmaps4rails.build_markers(@results) do |listing, marker|
       marker.lat listing.latitude
       marker.lng listing.longitude
